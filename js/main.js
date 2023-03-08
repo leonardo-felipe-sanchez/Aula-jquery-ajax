d9abec87086cd0d9b133250e5ddf365a423f198a
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2023-03-08 13:09:20 -0300

    finalizado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 
 function consultaCEP(){
- $.ajax({
-url: "https://viacep.com.br/ws/14800390/json/",
+
+//var mean = document.getElementById("mean");
+var cep = document.getElementById("idcep").value;
+var url = `https://viacep.com.br/ws/${cep}/json/`
+console.log(cep);
+console.log(url);
+    $.ajax({
+url: url,
 type: "GET",
-sucess: function(response){
+success: function(response){
     console.log(response);
+    //console.log(response.bairro);
+
+    $("#cep").html(response.cep);
+    $("#logradouro").html(response.logradouro);
+    $("#bairro").html(response.bairro);
+    $("#localidade").html(response.localidade);
+    $("#uf").html(response.uf);
+
+    mean.innerHTML = `${response.cep}</br> ${response.logradouro} </br> ${response.bairro}</br> ${response.localidade}</br> ${response.uf}`
 }
  })   
 } 
